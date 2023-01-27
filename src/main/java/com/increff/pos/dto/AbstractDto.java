06002e273313f63ef1e81ccf0450f3cf4c501cb6
Satyam Kumar Pathak <psatyam3006@gmail.com>
2023-01-27 15:00:40 +0530

    Fixed a serious bug

--- src/main/java/com/increff/pos/dto/AbstractDto.java
+++ src/main/java/com/increff/pos/dto/AbstractDto.java
@@ -1,13 +1,37 @@
 package com.increff.pos.dto;
 
 import com.increff.pos.api.ApiException;
+import org.springframework.stereotype.Service;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.lang.reflect.ParameterizedType;
 import java.util.Objects;
+import java.util.Set;
 
-public class AbstractDto {
+@Service
+public abstract class AbstractDto<T> {
+
+    Class<T> clazz;
+
+    public AbstractDto() {
+        this.clazz = (Class) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
     public void checkNull(Object object, String message) throws ApiException {
         if (Objects.isNull(object)) {
             throw new ApiException(message);
+        }
+    }
+
+    protected void validate(T form) throws ApiException {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(form);
+        for (ConstraintViolation<T> violation : violations) {
+            throw new ApiException(violation.getMessage());
         }
     }
 
